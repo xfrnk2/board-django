890d6c6f24b2914f89dfd2e084f61d1353289f14
xfrnk2 <xfrnk2@gmail.com>
2021-05-28 00:00:03 +0900

    프로젝트 내용 제거

--- myboard/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('userinfo/', views.userinfo),
-    #path('users/', include('users.urls')),
-    path('mypage/', views.mypage),
-]
