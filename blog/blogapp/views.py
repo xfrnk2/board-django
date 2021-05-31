53be4df3c88ff6548e44fc5ec2b93e7f2a87d2c4
xfrnk2 <xfrnk2@gmail.com>
2021-05-31 23:52:09 +0900

    제너릭 뷰를 사용한 연/월/일/시 표현 따라하기

---
+++ blog/blogapp/views.py
@@ -0,0 +1,37 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
+from django.views.generic.dates import DayArchiveView, TodayArchiveView
+from blogapp.models import Post
+
+
+class PostLV(ListView):
+    model = Post
+    template_name = 'blogapp/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+class PostDV(DetailView):
+    model = Post
+
+class PostAV(ArchiveIndexView):
+    model = Post
+    data_field = 'modify_dt'
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    make_object_list = True
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+class PostDAV(DayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+class PostTAV(TodayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+# Create your views here.
